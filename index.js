f2827d93dc6f72947b6c177fb2c681e17bb7e5fb
Michael Iodice <miodice3@gmail.com>
2021-01-20 14:45:41 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
 // Your code here
+
+function mapToNegativize(ray){
+    let returnRay = []
+    ray.forEach(function(index){
+        returnRay.push(index * -1)
+    })
+    return returnRay
+}
+
+function mapToNoChange(ray){
+    return ray
+}
+
+function  mapToDouble(ray){
+    let returnRay = []
+    ray.forEach(function(index){
+        returnRay.push(index * 2)
+    })
+    return returnRay
+}
+
+function  mapToSquare(ray){
+    let returnRay = []
+    ray.forEach(function(index){
+        returnRay.push(index ** 2)
+    })
+    return returnRay
+}
+
+function reduceToTotal(ray, base=0){
+    let value = base
+    ray.forEach(function(index){
+        value += index
+    })
+    return value
+}
+
+function reduceToAllTrue(ray){
+    let value = true
+    ray.forEach(function(index){
+        if (index == false){
+            return value = false
+        }
+    })
+    return value
+}
+
+function reduceToAnyTrue(ray){
+    let value = false
+    ray.forEach(function(index){
+        if (index == true){
+            return value = true
+        }
+    })
+    return value
+}
+
+
+
+
+
+
+
+
+
+
+
+// reduceToAnyTrue returns true when a true value is present
+// reduceToAnyTrue returns false when no true value is present
